faadeaeb3a5199a455b259991d3a7e87f50397e9
Raghav Pandit <panditraghav18@gmail.com>
2024-06-09 22:42:31 +0530

    Added a navigation menu for mobile

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,19 @@
+"use client";
+import { cn } from "@/lib/utils";
 import {
   SearchIcon,
   UserIcon,
   HeartIcon,
   ShoppingBagIcon,
   ArrowRightIcon,
+  XIcon,
+  MenuIcon,
 } from "lucide-react";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Header() {
+  const [navOpen, setNavOpen] = useState(false);
   return (
     <div className="container z-50 mx-auto flex items-center justify-between bg-my-bg-cream px-3 py-8 md:w-full md:bg-transparent md:px-0">
       <h1 className="medium font-sans text-xl font-medium">
@@ -47,14 +53,80 @@ export default function Header() {
           </IconButton>
         </div>
       </div>
+      <button className="md:hidden" onClick={() => setNavOpen(true)}>
+        <MenuIcon />
+      </button>
+      <MobileNavbar
+        open={navOpen}
+        onClose={() => {
+          setNavOpen(false);
+        }}
+      />
     </div>
   );
 }
 
-function IconButton({ children }: { children: React.ReactNode }) {
+function IconButton({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
   return (
-    <button className="flex items-center justify-center rounded-full bg-[#4F684A] p-2 text-white">
+    <button
+      className={cn(
+        "flex items-center justify-center rounded-full bg-[#4F684A] p-2 text-white",
+        className,
+      )}
+    >
       {children}
     </button>
+  );
+}
+
+function MobileNavbar({
+  open,
+  onClose,
+}: {
+  open?: boolean;
+  onClose: () => void;
+}) {
+  return (
+    <nav
+      data-open={open}
+      className="absolute right-0 top-0 z-50 h-screen w-[60%] translate-x-full bg-my-bg-cream px-8 py-8 transition-all duration-300 data-[open='true']:translate-x-0 data-[open='true']:slide-in-from-left md:hidden"
+    >
+      <div className="flex justify-end">
+        <button onClick={onClose}>
+          <XIcon />
+        </button>
+      </div>
+      <div className="my-4 flex gap-x-2">
+        <IconButton className="bg-transparent text-primary-foreground-800">
+          <SearchIcon size={20} className="" />
+        </IconButton>
+        <IconButton className="bg-transparent text-primary-foreground-800">
+          <UserIcon size={20} className="" />
+        </IconButton>
+        <IconButton className="bg-transparent text-primary-foreground-800">
+          <HeartIcon size={20} className="" />
+        </IconButton>
+        <IconButton className="bg-transparent text-primary-foreground-800">
+          <ShoppingBagIcon size={20} className="" />
+        </IconButton>
+      </div>
+      <ul className="space-y-8 font-medium">
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/groceries">Groceries</Link>
+        </li>
+        <li>
+          <Link href="/pages">Pages</Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -28,6 +28,9 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("tailwindcss-animate"),
+    // ...
+  ],
 };
 export default config;
